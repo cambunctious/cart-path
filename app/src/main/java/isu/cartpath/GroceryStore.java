e0bf781b962070e2e3c2b8d31e787c7698d3e7d1
rhnguye <rhnguye@ilstu.edu>
2015-11-29 01:47:00 -0600

    GroceryStore layout changes

--- app/src/main/java/isu/cartpath/GroceryStore.java
+++ app/src/main/java/isu/cartpath/GroceryStore.java
@@ -3,9 +3,13 @@ package isu.cartpath;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.*;
+import android.widget.ListAdapter;
 
 import java.util.ArrayList;
 
@@ -26,19 +30,56 @@ public class GroceryStore extends AppCompatActivity
 
         Intent intent = getIntent();
         String[] categories = GroceryKnowledge.getInstance(this).getCategoriesArray();
+        ListView list = (ListView) findViewById(R.id.stores);
+        list.setAdapter(new ArrayAdapter<String>(this, R.layout.category, categories){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null) {
+                    convertView = LayoutInflater.from(getContext()).inflate(R.layout.category, null);
+                    TextView textview = (TextView)convertView.findViewById(R.id.category_name);
+                    textview.setText(getItem(position));
+                }
+                return convertView;
+            }
+        });
+
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        getMenuInflater().inflate(R.menu.menu_master_list,menu);
+        getMenuInflater().inflate(R.menu.menu_edit_store, menu);
         return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.cart_path_view)
+        {
+            Intent cartPathIntent = new Intent(this, CartPath.class);
+            startActivity(cartPathIntent);
+            return true;
+        }
+        if (id == R.id.aisle_view)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
 
     public void createStore(View view)
     {
+        final EditText editText = (EditText) findViewById(R.id.newStore);
 
+        //helper.addItem();
     }
 
     public void editStore(View view)
